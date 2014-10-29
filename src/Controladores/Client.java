2b92de077ed388169bdd821d4333ca54b7a7a947
israelve <lwjirl@gmail.com>
2014-10-29 18:50:29 -0430

    falto

--- src/Controladores/Client.java
+++ src/Controladores/Client.java
@@ -1,13 +1,27 @@
 
 package Controladores;
 
+import Capa_Persistencia.Oracle;
+import DTO.ClienteBean;
 import java.io.IOException;
 
 public class Client {
 
     public static void main(String[] args) throws IOException{
+        
+        
+        Oracle ora= new Oracle();
+        ClienteBean perso = new ClienteBean();
+        perso = ora.get_cliente(20911444);
+        System.out.println(perso.getNombre());
+        
+        ora.add_alquiler(1, null,null, 1, 20911444);
+        
         UCVBuster b = UCVBuster.Instance();
         b.seleccionarOpcion(1);
+        
+        
+        
     }
 
 }
